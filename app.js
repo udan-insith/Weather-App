f5823e6bd0e24b7fc6bdda013c04b5a65d5a76c1
udan-insith <udaninsith23613@gmail.com>
2026-03-10 15:30:45 +0530

    created JS file

---
+++ app.js
@@ -0,0 +1,8 @@
+const apiKey = "89bedd80a0646b9e1f7641b2119b32fc"
+const weatherData = document.getElementById("weather-data")
+const cityInputEl = document.getElementById("city-input")
+const formEl = document.querySelector("form")
+
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault()
+})
